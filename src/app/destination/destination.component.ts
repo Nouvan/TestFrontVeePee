337b666a89f3e6ac6f1a1502700b464aaac03f5b
Nouvan <nouvan.sodak@gmail.com>
2020-07-22 12:06:06 +0200

    Fix the replaceALL + assets links

--- src/app/destination/destination.component.ts
+++ src/app/destination/destination.component.ts
@@ -16,7 +16,7 @@ export class DestinationComponent implements OnInit {
   destinationFileName() {
     return this.destination.label
       .toUpperCase()
-      .replaceAll(" ", "_")
+      .replace(/ /g, "_")
       .normalize("NFD")
       .replace(/[\u0300-\u036f]/g, "");
   }
