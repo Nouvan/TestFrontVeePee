59303086b0a8662bbeaeda524865c613e17587e0
Nouvan <nouvan.sodak@gmail.com>
2020-07-22 05:11:56 +0200

    VeePee Front Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as destinations from "./destinations.json";
+import { trigger, style, animate, transition } from "@angular/animations";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  animations: [
+    trigger("enterAnimation", [
+      transition(":enter", [
+        style({ transform: "scale(0.5)", opacity: 0 }), // initial
+        animate(
+          "1s cubic-bezier(.8, -0.6, 0.2, 1.5)",
+          style({ transform: "scale(1)", opacity: 1 })
+        ), // final
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'TestFrontVeePee';
+  title = "TestFrontVeePee";
+
+  /**
+   * List of destinations
+   */
+  public destinations = destinations.destinations;
+
+  /**
+   * List of available countries
+   */
+  public countries;
+
+  public search = "ALL";
+
+  ngOnInit() {
+    const countries = new Set();
+    this.destinations.map((x) => {
+      countries.add(x.country);
+    });
+
+    this.countries = [...countries].sort();
+    console.log(this.search);
+  }
 }
---
+++ src/app/destination/destination.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { trigger, style, animate, transition } from "@angular/animations";
+
+@Component({
+  selector: "app-destination",
+  templateUrl: "./destination.component.html",
+  styleUrls: ["./destination.component.scss"],
+})
+export class DestinationComponent implements OnInit {
+  @Input() destination;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  destinationFileName() {
+    return this.destination.label
+      .toUpperCase()
+      .replaceAll(" ", "_")
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
+  }
+}
